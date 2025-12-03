5366ee083cda5cfd3cc0e8f6be049ce7501f884b
dijez <khadijaahmad2k21@gmail.com>
2025-12-03 12:37:38 +0100

    update

--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -1,9 +1,18 @@
 import React, {useState} from "react";
-
-
+import { ImageProps } from "@/interfaces";
+import ImageCard from "@/components/layouts/common/ImageCard";
+// import { useState } from "react";
 
 
 const Home: React.FC = () => {
+
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
+    []
+  );
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
 
   const handleGenerateImage = async () => {
     console.log("Generating Images");
@@ -12,7 +21,7 @@ const Home: React.FC = () => {
 
 
   return (
-     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
+      <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
@@ -22,6 +31,8 @@ const Home: React.FC = () => {
         <div className="w-full max-w-md">
           <input
             type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
             className="w-full p-3 border border-gray-300 rounded-lg mb-4 text-black"
           />
@@ -29,11 +40,16 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
+            {/* {
+              isLoading ? "Loading..." : "Generate Image"
+            } */}
             Generate Image
           </button>
         </div>
+
+        {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
-     </div>
+    </div>
   )
 
 }
